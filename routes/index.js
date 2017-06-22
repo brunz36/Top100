16c343035c004cc693af10c8bdf68a9a355d3322
Bruno Ribeiro <brunz36@gmail.com>
2017-06-22 13:38:53 -0400

    App using node.js and Apple API

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+const request = require('request');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  let _url = "https://rss.itunes.apple.com/api/v1/us/apple-music/top-albums/100/non-explicit/json";
+  request.get(_url, (error, response, body) => {
+    let _json = JSON.parse(body);
+    let _top100 = _json.feed.results;
+    res.render('index', { title: 'TIY Top 100 of the day', data: _top100});
+  });
+});
+
+module.exports = router;
